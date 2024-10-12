525116721af64825e4dafd576f0f8acd790afc92
Leena <leenahayat14@gmail.com>
2024-10-12 09:57:07 +0500

    Initial commit

---
+++ tut53.js
@@ -0,0 +1,42 @@
+console.log("This is Tutorial 53");
+
+//Without Functions
+
+// let names = "Leena ";
+// let name1 = "Nida ";
+// let name2 = "Aleena ";
+// console.log(name1 + "is a good girl");
+// console.log(name2 + "is a good girl");
+// console.log(name6 + "is a good girl");
+
+
+
+//Use of Functions
+function greet(nameis){
+    console.log(meetText +" "+nameis)
+    console.log(nameis + " is a good girl.");
+}
+let nameis = "Leena ";
+let namei1 = "Nida ";
+let namei2 = "Aleena ";
+let meetText="Hi";
+greet(namei1,meetText);
+greet(namei2,meetText);
+greet(nameis,meetText);
+
+//Use of Functions
+function greet(names,greetText){
+    console.log(greetText+" "+names)
+    console.log(names + " is a good girl.");
+}
+let names = "Leena ";
+let name1 = "Nida ";
+let name2 = "Aleena ";
+let greetText= "GoodMorning";
+greet(name1,greetText);
+greet(name2,greetText);
+greet(names,greetText);
+
+
+
+
